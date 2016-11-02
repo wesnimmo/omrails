cc3c41a00966fc5dcb9540f84a97bf67511955a5
Wesley Nimmo <wesnimmo@gmail.com>
2016-11-02 16:08:14 -0700

    Added BS styles to Links and Tweets and validations to tweets

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,7 @@
 class Link < ApplicationRecord
+
   belongs_to :user
+
+  validates :user, presence: true
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,8 @@
 class Tweet < ApplicationRecord
+
   belongs_to :user
+
+  validates :user, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+
 end
