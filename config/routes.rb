aa19e1b2aa36d0757da17a9f923dbf33408c0e59
Wesley Nimmo <wesnimmo@gmail.com>
2016-11-03 23:59:28 -0700

    Fixed logout users bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get "signup", to: 'devise/registrations#new'
+  end
+  
   get 'feed', to: 'feed#show'
 
   resources :users, only: :show, param: :username do
@@ -10,14 +19,6 @@ Rails.application.routes.draw do
 
   resources :links
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-
-  as :user do
-  	get "signin", to: 'devise/sessions#new'
-  	delete "signout", to: 'devise/sessions#destroy'
-  	get "signup", to: 'devise/registrations#new'
-  end
   
   root 'pages#home'
   get 'about', to: 'pages#about'
