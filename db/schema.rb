5d8795339f7fac4897719dde11f8e4a605bc4567
Wesley Nimmo <wesnimmo@gmail.com>
2016-11-05 14:36:25 -0700

    Added image upload to tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105193342) do
+ActiveRecord::Schema.define(version: 20161105210958) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -50,8 +50,12 @@ ActiveRecord::Schema.define(version: 20161105193342) do
   create_table "tweets", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
